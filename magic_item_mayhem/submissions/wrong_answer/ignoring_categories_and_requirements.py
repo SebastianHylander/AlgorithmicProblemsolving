df320cb3c14cf3b685766c7eb4d5b2d65eee5314
Robin Ask Rohr Halberg <halb@itu.dk>
2024-05-15 18:53:12 +0200

    Added more

---
+++ magic_item_mayhem/submissions/wrong_answer/ignoring_categories_and_requirements.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+n,m = map(int,input().split())
+print(min(n*3,m))
---
+++ magic_item_mayhem/submissions/wrong_answer/reusing_items.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+n,m = map(int, input().split())
+
+players = dict()
+
+for _ in range(n):
+    players[input().strip()] = [0]*5
+
+for _ in range(m):
+    inp = input().split()
+    cat = int(inp[0])
+    for name in inp[1:]:
+        players[name][cat] = 1
+
+print(sum(min(3,sum(x)) for x in players.values()))
