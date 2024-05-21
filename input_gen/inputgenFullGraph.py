d245c1ec88032e8d40f8f26b79489f2f1cad22a5
Robin Ask Rohr Halberg <robin.hal6erg@gmail.com>
2024-05-21 16:22:47 +0200

    Added generator for full graph input

---
+++ input_gen/inputgenFullGraph.py
@@ -0,0 +1,39 @@
+import random, string, edmonds_karp_for_gen
+
+# Replace 
+# 1 <= N <= 100 
+N = 100 
+# 1 <= M <= 600
+M = 600
+
+filename = 'magic_item_mayhem/data/secret/' + str(N) + '_' + str(M) + '_Full'
+
+fin = open(filename + '.in', "x")
+
+fin.write(str(N) + ' ' + str(M) + '\n')
+
+# Generate names
+names = set()
+while len(names) < N:
+    length = random.randint(3,10)
+    name = ''.join(random.choices(string.ascii_lowercase, k=length))
+    names.add(name)
+
+names = list(names)
+
+for name in names:
+    fin.write(name + '\n')
+
+# Generate items
+for _ in range(M):
+    cat = random.randint(1,5)
+    fin.write(str(cat) + ' ' + ' '.join(names) + '\n')
+
+fin.close()
+
+# Run algorithm and write answer file
+res = edmonds_karp_for_gen.run(filename)
+
+fans = open(filename + '.ans', "x")
+fans.write(str(res) + '\n')
+fans.close()
